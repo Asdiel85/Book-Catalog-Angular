9d91c34dc5ad99a34adc9be1710a7bb96b78d265
Martin Hristov <mghristov85@gmail.com>
2024-03-23 11:52:08 +0200

    added register

--- Book-Catalog/src/app/auth/authService/auth-service.service.ts
+++ Book-Catalog/src/app/auth/authService/auth-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AUTH, BASE_URL, LOGIN } from 'src/app/constants/constants';
-import { UserLogin } from 'src/app/types/user';
+import { AUTH, BASE_URL, LOGIN, REGISTER } from 'src/app/constants/constants';
+import { UserLogin, UserRegister } from 'src/app/types/user';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +12,10 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<UserLogin>(`${BASE_URL}${AUTH}${LOGIN}`, { email, password })
+  }
+
+  register(email: string, password: string, repeatPassword: string) {
+    return this.http.post<UserRegister>(`${BASE_URL}${AUTH}${REGISTER}`, { email, password, repeatPassword })
   }
 
   getUser(): boolean {
--- Book-Catalog/src/app/types/user.ts
+++ Book-Catalog/src/app/types/user.ts
@@ -11,3 +11,9 @@ export interface UserLogin {
     email: string,
     password: string
 }
+
+export interface UserRegister {
+    email: string,
+    password: string,
+    repeatPassword: string
+}
