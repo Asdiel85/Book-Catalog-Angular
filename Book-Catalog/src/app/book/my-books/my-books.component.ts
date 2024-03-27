fcb2f4b4d3dd3a93374f85f3ac1999a503163896
Asdiel85 <mghristov85@gmail.com>
2024-03-27 22:34:03 +0200

    added switchmap on my books

--- Book-Catalog/src/app/book/my-books/my-books.component.ts
+++ Book-Catalog/src/app/book/my-books/my-books.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../book-service/book-service.service';
 import { Book } from 'src/app/types/book';
+import { AuthService } from 'src/app/auth/authService/auth-service.service';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-my-books',
   templateUrl: './my-books.component.html',
-  styleUrls: ['./my-books.component.css']
+  styleUrls: ['./my-books.component.css'],
 })
 export class MyBooksComponent implements OnInit {
-  books:Book[] = []
-  constructor(private bookService: BookService) {}
+  books: Book[] = [];
+  constructor(
+    private bookService: BookService,
+    private authService: AuthService
+  ) {}
   ngOnInit(): void {
-    this.getMyBooks()
+   this.getMyBooks().subscribe(data => {
+    this.books = data;
+   })
   }
 
-  getMyBooks() : void {
-    this.bookService.getUserBooks('65d34fbec0338e4a312bab78').subscribe((data) => {
-      this.books = data
-     })
+  getMyBooks(): Observable<Book[]> {
+    return this.authService
+      .getloggedUserId()
+      .pipe(switchMap((id) => this.bookService.getUserBooks(id)))
   }
 }
